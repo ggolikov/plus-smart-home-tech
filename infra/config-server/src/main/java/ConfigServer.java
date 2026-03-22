d9d52cba3fc0f678bd2889582c6261fd0eb84176
Grigory Golikov <golikov@nextbi.ru>
2026-03-22 08:56:47 +0300

    Add config-server

---
+++ infra/config-server/src/main/java/ConfigServer.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+
+}
